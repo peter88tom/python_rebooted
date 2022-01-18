f1e8b54a83a305bfece1b87dc1094dab6e7c8d37
peter88tom <peter88tom@gmail.com>
2022-01-18 15:30:29 +0200

    Working with list

---
+++ data_structure/list.py
@@ -0,0 +1,27 @@
+"""
+Python list is just like array which is an ordered collection of data
+Very flexible as the items in a list do not need to be of the same type
+"""
+List = [1, 2, 3, "one two three", 4.6]
+print(List)
+
+"""
+List elements can be accessed by index
+In python index start at 0
+"""
+# Here we want to print 2 from the list, we can access this by list[1]
+print(List[1])
+
+
+"""
+Python list operation
+"""
+# Create a list with
+# The use of multiple values
+another_list = ["Nerd", "For", "Nerd"]
+print("\nList containing multiple values: ")
+print(another_list)
+
+# Access a element using negative indexing
+print(another_list[-1])
+
