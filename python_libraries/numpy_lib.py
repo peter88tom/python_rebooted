66c76ba30e8efc33dcb35463c511383718cb371f
peter88tom <peter88tom@gmail.com>
2022-01-18 21:33:10 +0200

    Introduction to numpy

---
+++ python_libraries/numpy_lib.py
@@ -0,0 +1,15 @@
+"""
+NumPy is a general-purpose-array-processing package.
+It provide a high performance multidimensional array object,
+"""
+import numpy as np
+
+# Creating array object
+arr = np.array([[1, 2, 3],
+               [4, 2, 5]])
+
+# Print type of array
+print(f"Array is of type: {type(arr)}")
+print(f"Size of array is: {arr.size}")
+print(f"Shape of array is: {arr.shape}")
+print(f"Number of")
