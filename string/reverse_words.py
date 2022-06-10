042ca1c3905816c809a724434a9d797a4de0dd61
peter88tom <peter88tom@gmail.com>
2022-06-10 13:19:37 +0200

    Reverse words

---
+++ string/reverse_words.py
@@ -0,0 +1,18 @@
+"""
+We are given a string and we need to reverse words of a given string
+"""
+word = "The lazy dog is trying to jump a wall"
+# expected out: wall a jump to trying is dog lazy The
+
+words = word.split()[::-1]
+
+reversed_word = []
+
+for w in words:
+  # append reverse words
+  reversed_word.append(w)
+
+# Join the split words
+print(" ".join(reversed_word))
+
+
