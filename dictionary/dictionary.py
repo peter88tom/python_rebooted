222dd118af646a8301906c57a45d4ae4e473c884
peter88tom <peter88tom@gmail.com>
2022-06-12 08:52:51 +0200

    python dict recap

---
+++ dictionary/dictionary.py
@@ -0,0 +1,46 @@
+"""
+Dictionary is key value array
+"""
+
+student = {"first_name": "Zara", "age": "14", "college": "Zara College"}
+print(student)
+
+print("You access value using keys")
+print(student["first_name"])
+print("\n")
+
+print("Updating dictionary element")
+student["age"] = 15
+print(student)
+print("\n")
+
+print("Add new dictionary element")
+student["last_name"] = "Booker"
+print(student)
+print("\n")
+
+
+print("Deleting dictionary element")
+del student["last_name"]
+print(student)
+print("\n")
+
+print("Copying a dictionary(Shallow copy)")
+new_student = student.copy()
+print(new_student)
+print("\n")
+
+print("Return value or default if key not in dictionary")
+print(student.get("age"))
+print("\n")
+
+print("Return list of dictionary (key, value) tuple pairs ")
+print(student.items())
+print("\n")
+
+print("Deleting all elements in a dictionary")
+student.clear()
+print(student)
+
+
+
